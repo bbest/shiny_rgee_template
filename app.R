3e054b4f31d9d140b243db15b982e1462eaaea57
Ben Best <ben@ecoquants.com>
2023-09-12 14:44:27 +0000

    rgee app working after py reqs

--- app.R
+++ app.R
@@ -1,32 +1,68 @@
+
+# https://github.com/csaybar/shiny_rgee_template/blob/main/app.R
+Sys.setenv(
+  EARTHENGINE_GCLOUD = "/usr/bin/gcloud",
+  EARTHENGINE_PYTHON = "/opt/venv/rgee/bin/python",
+  EARTHENGINE_ENV    = "rgee")
+
+librarian::shelf(
+  leaflet, rgee, reticulate, shiny)
+# source("utils.R")
+
+# ee_install(
+#   py_env = "rgee",
+#   earthengine_version = ee_version(),
+#   python_version = "3.8",
+#   confirm = interactive())
+
+# rgee::ee_check()
+# ◉  Python version
+#   ✔ [Ok] /opt/venv/rgee/bin/python v3.8
+# ◉  Python packages:
+#   ✔ [Ok] numpy
+#   ✔ [Ok] earthengine-api
+
+# rgee:::ee_check_init()
+# $earthengine_version
+# [1] "0.1.368"
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+# $ee_utils
+# Module(ee_utils)
+
+ee_Initialize(
+  user          = "ben@ecoquants.com",
+  project       = "eq-am-fine")
+# Successfully saved authorization token.
+# ✔ Initializing Google Earth Engine:  DONE!
+# ✔ Earth Engine account: projects/eq-am-fine/assets/fao_areas
+# ✔ Python Path: /opt/venv/rgee/bin/python
+
+# srtm <- ee$Image("USGS/SRTMGL1_003")
 #
-# Find out more about building applications with Shiny here:
+# viz <- list(
+#   max = 4000,
+#   min = 0,
+#   palette = c("#000000","#5AAD5A","#A9AD84","#FFFFFF"))
 #
-#    http://shiny.rstudio.com/
-#
-library(leaflet)
-library(shiny)
-library(rgee)
+# Map$addLayer(
+#   eeObject = srtm,
+#   visParams =  viz,
+#   name = 'SRTM')
 
-source("utils.R")
+# TODO: use Google service account
+cloud_api_key = "/share/data/eq-am-fine-98c68d6f5f96.json"
 
+# [install gcloud CLI](https://cloud.google.com/sdk/docs/install#deb)
+#   sudo apt-get update
+#   sudo apt-get install apt-transport-https ca-certificates gnupg curl sudo
+#   echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+#   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+#   sudo apt-get update && sudo apt-get install google-cloud-cli
 
-# 1. Set rgee ----------------------------------------------------------
-if (!reticulate::virtualenv_exists("rgee_py")) {
-    set_rgee_dependencies()
-}
-
-
-# 2. Run again ee_Initialize, after a long period of inactivity. ----------
-tryCatch(
-    expr = ee$Image(0),
-    error = function(e) {ee_Initialize()}
-)
-
-
-
+# /usr/bin/gcloud init
+# ben@ecoquants.com
+# eq-am-fine
+# Created a default .boto configuration file at [/home/admin/.boto]
 
 # 3. DEFINE HERE YOUR APP -------------------------------------------------
 ui <- fluidPage(
@@ -35,15 +71,15 @@ ui <- fluidPage(
     actionButton("recalc", "Add SRTM Global Map")
 )
 server <- function(input, output, session) {
-    dem_map <- eventReactive(input$recalc, {
-        Map$addLayer(ee$Image('srtm90_v4'), list(min = 0, max = 1000))
-    }, ignoreNULL = FALSE)
+
+    dem_map <- eventReactive(
+      input$recalc,
+      {
+        Map$addLayer(ee$Image('srtm90_v4'), list(min = 0, max = 1000)) },
+      ignoreNULL = FALSE)
 
     output$mymap <- renderLeaflet({
         dem_map()
     })
 }
 shinyApp(ui = ui, server = server)
-# -------------------------------------------------------------------------
-
-
